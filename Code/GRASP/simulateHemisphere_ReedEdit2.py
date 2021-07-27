9dfb8f158c90bf1ae0be4b1f2fb51ee63c9faed4
cdeleon <cdeleon@email.arizona.edu>
2021-07-26 21:36:00 -0700

    GRASP

--- Code/GRASP/simulateHemisphere_ReedEdit2.py
+++ Code/GRASP/simulateHemisphere_ReedEdit2.py
@@ -3,18 +3,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 import datetime as dt
 
-#Global Font Size 
-plt.rcParams.update({'font.size': 22}
+
 
 # Path to the YAML file you want to use for the aerosol and surface definition
-# fwdModelYAMLpath = '/Users/wrespino/Synced/Local_Code_MacBook/MADCAP_Analysis/ACCP_ArchitectureAndCanonicalCases/settings_FWD_IQU_POLAR_1lambda.yml'
-fwdModelYAMLpath = '/Users/wrespino/Downloads/settings_Dust.yml'
-
+#fwdModelYAMLpath = '/home/cdeleon/ULTRASIP/Code/GRASP/SettingFiles/settings_BiomassBurning.yml'
+fwdModelYAMLpath = '/home/cdeleon/ULTRASIP/Code/GRASP/SettingFiles/settings_BiomassBurning.yml'
 # paths to your GRASP binary and kernels (replace everything up to grasp_open with the path to your GRASP repository)
-# binPathGRASP = '/home/cdeleon/grasp/build/bin/grasp'
-# krnlPathGRASP = '/home/cdeleon/grasp/src/retrieval/internal_files'
-binPathGRASP = '/Users/wrespino/Synced/Local_Code_MacBook/grasp_open/build/bin/grasp'
-krnlPathGRASP = '/Users/wrespino/Synced/Local_Code_MacBook/grasp_open/src/retrieval/internal_files'
+binPathGRASP = '/home/cdeleon/grasp/build/bin/grasp'
+krnlPathGRASP = '/home/cdeleon/grasp/src/retrieval/internal_files'
 
 ttlStr = 'Dust (multi scattering)' # Top Title
 wvStrFrmt =  '($%4.2f\\mu m$)' # Format of Y-axis labels 
@@ -44,12 +40,17 @@ gr = graspRun(pathYAML=fwdModelYAMLpath, releaseYAML=True, verbose=True)
 gr.addPix(nowPix)
 gr.runGRASP(binPathGRASP=binPathGRASP, krnlPathGRASP=krnlPathGRASP)
 print('AOD at %5.3f μm was %6.4f.' % (gr.invRslt[0]['lambda'][-1],gr.invRslt[0]['aod'][-1]))
-
+#Global Font Size 
+plt.rcParams.update({'font.size': 14})
 # hemisphere plotting code
 Nwvl = len(wvls)
 # print(Nwvl)
-fig, ax = plt.subplots(2*Nwvl-1, 2, subplot_kw=dict(projection='polar'), figsize=(6,6+3*(Nwvl-1)))
-if Nwvl == 1: ax = ax[None,:]
+fig, ax = plt.subplots(2*Nwvl-1, 2, subplot_kw=dict(projection='polar'))#, figsize=(6,6+3*(Nwvl-1)))
+#ax = plt.plot(dict(projection='polar'))#, figsize=(6,6+3*(Nwvl-1)))
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='polar')
+if Nwvl == 1: 
+    ax = ax[None,:]
 pxInd = 0
 r = gr.invRslt[pxInd]['vis'][:,0].reshape(Nazimth, Nvza)
 if vza.max()>90: r = 180 - r
@@ -66,42 +67,59 @@ for i in range(2):
             if i==1: # we need to find DoLP
                 Q = gr.invRslt[pxInd]['fit_Q'][:,l]
                 U = gr.invRslt[pxInd]['fit_U'][:,l]
-                data[-1] = (np.sqrt(Q**2+U**2)/data[-1]) * 100
+                data[-1] = (np.sqrt(Q**2+U**2)/data[-1]) #* 100
+                clrMin = 0#data[-1].min(0)
+                clrMax = 1#1data[-1].max(100)
             labels.append(wvStr)
+            titlestr = 'DoLP [decimal]'
             if i==0: 
                 data[-1] = np.log10(data[-1])
                 clrMin = data[-1].min()
                 clrMax = data[-1].max()
-            else:
-                clrMin = data[-1].min(0)
-                clrMax = data[-1].max(100)
-            clrMap = clrMapReg
+                titlestr='log(Reflectance)'
+            #else:
+               # clrMin = data[-1].min(0)
+               # clrMax = data[-1].max(100)
+            #clrMap = clrMapReg
         else: # this is a difference plot      
 #             if i==1:
             data.append(data[l-Nwvl+1] - data[0])
             labels.append(labels[l-Nwvl+1] + " – " + labels[0])
+            titlestr = 'Difference'
+            wvStr = '(0.55um - 0.34um)'
   #           else:
 #                 data.append(1 - (data[l-Nwvl+1]/data[0]))
 #                 labels.append("1 - " + labels[l-Nwvl+1] + "/" + labels[0])
-            clrMax = 50
+            clrMax = 0.5
             clrMin = -clrMax
-            clrMap = clrMapDiff
+            #clrMap = clrMapDiff
+        #fig=plt.figure()
+        fig = plt.figure()
+        ax = plt.subplots(subplot_kw=dict(projection='polar'))#, figsize=(6,6+3*(Nwvl-1)))
         v = np.linspace(clrMin, clrMax, 200, endpoint=True)
         ticks = np.linspace(clrMin, clrMax, 3, endpoint=True)
         data2D = data[-1].reshape(Nazimth, Nvza)
         dataFullHemisphere = np.vstack([data2D, np.flipud(data2D)]) # mirror symmetric about principle plane
-        c = ax[l,i].contourf(theta, r, dataFullHemisphere, v, cmap=clrMap)
-        ax[l,i].plot(np.pi, sza, '.',  color=[1,1,0], markersize=20)
-        ax[l,i].set_ylim([0, r.max()])
-        if i==0: ax[l,i].set_ylabel(labels[-1], labelpad=30)
+        c = plt.contourf(theta, r, dataFullHemisphere, v, cmap='hsv')
+        plt.title(titlestr)
+        plt.ylabel(wvStr, labelpad=30)
+        plt.plot(np.pi, sza, '.',  color=[1,1,0], markersize=20)
+#ax.set_ylim([0, r.max()])
+        #if i==0: 
+#ax.set_ylabel(labels[-1], labelpad=80)
 
-cb = plt.colorbar(c, orientation='horizontal', ax=ax[l,i], ticks=ticks)
-cb = plt.colorbar(c, orientation='horizontal', ax=ax[l,i], ticks=ticks)
-cb = plt.colorbar(c, orientation='horizontal', ax=ax[l,i], ticks=ticks)
+        cb = plt.colorbar(c, orientation='vertical', ticks=ticks,pad=0.1)
+#cb = plt.colorbar(c, orientation='horizontal', ax=ax[l,i], ticks=ticks)
+#cb = plt.colorbar(c, orientation='horizontal', ax=ax[l,i], ticks=ticks)
 
-ax[0,0].set_title('log10(Reflectance)')
-ax[0,1].set_title('DoLP [%]')
+#ax[0,0].set_title('log10(Reflectance)')
+#ax[0,1].set_title('DoLP [%]')
 # plt.suptitle(ttlStr)
-plt.tight_layout(rect=[0.01, 0.01,0.98, 0.98])
+#plt.tight_layout(rect=[0.5, 0.5,0.98, 0.98])
+
+#plt.tight_layout(rect=[0.01, 0.01,0.98, 0.98])
 plt.ion()
 plt.show()
+
+fig=plt.figure()      
+plt.hist(data[2])
