50fde371572f711d7bb895a17e9db6edc3f8cef3
AtkinHyatt <atkindavidhyatt@email.arizona.edu>
2021-08-20 10:55:58 -0700

    Added support for printing Stoke's parameters as well as a function for printing data

--- Code/Taking_Data!!!/TestIntegratingSphere.m
+++ Code/Taking_Data!!!/TestIntegratingSphere.m
@@ -2,10 +2,11 @@
 % offset of 0 as well as offset of some arbitrary delta.
 %
 % Written by Atkin Hyatt 08/06/2021
-% Last modified by Atkin Hyatt 08/10/2021
+% Last modified by Atkin Hyatt 08/20/2021
 
-% Initialize
-%initializeUV
+%% Initialize
+%initializeUV;
+stop(vid)
 expo = input('Exposure time in seconds? ');
 src.ExposureTime = expo;
 
@@ -31,7 +32,7 @@ image = zeros(2*L,512,512);
 for N = 1 : L
     Move_motor(deg(N),ELL14);
 
-    pause(1)
+    pause(2)
     
     image(N,:,:) = UV_data(vid,framesPerTrigger) - dark;
     disp('image taken')
@@ -40,6 +41,7 @@ end
 % "Tilt" instrument to delta
 fopen(ELL14);
 h = dec2hex(round(398.222222222222 * delta + hex2dec(home)), 8);
+trueDel = TranslateELL14(query(ELL14, "0go"));
 fprintf(ELL14, "%s", "0so" + h);
 fclose(ELL14);
 
@@ -47,7 +49,7 @@ fclose(ELL14);
 for N = 1 : L
     Move_motor(deg(N),ELL14);
     
-    pause(1)
+    pause(2)
     
     image(N + L,:,:) = UV_data(vid,framesPerTrigger) - dark;
     disp('image taken')
@@ -84,8 +86,9 @@ for N = 1 : length(del)
    end
 end
 
-global filename
-filename = [saving_dir '' date '_' time '_' ex '_' del '.h5'];
+global filename; global file;
+file = [date '_' time '_' ex '_' del '.h5'];
+filename = [saving_dir '' file];
 
 % Create h5
 h5create(filename,'/measurement/images',size(image));
@@ -94,46 +97,12 @@ h5write(filename,'/measurement/images',image);
 % Write attibutes to directory
 h5writeatt(filename,'/measurement/images/','date', date);
 h5writeatt(filename,'/measurement/images/','time', time);
-h5writeatt(filename,'/measurement/images/','delta', delta);
+h5writeatt(filename,'/measurement/images/','delta', trueDel);
 h5writeatt(filename,'/measurement/images/','exposure', expo);
 
-%% Data processing
+%% Process data
 addpath('C:\ULTRASIP\Code\Data_Analysis');
 ULTRASIP_IntegratingSphere_Analysis;
 
-for N = 1 : length(correctImage(:,323,275))/4
-    stdevDOLP(N) = std(reshape(DoLP, 1, 323*275));
-    stdevAOLP(N) = std(reshape(AoLP, 1, 323*275));
-end
-
 %% Plot data
-% Show intermediate images
-
-for n = 1 : length(correctImage(:,323,275))/4
-    N = 4*n - 3;
-    figure(n)
-    subplot(2,2,1);
-    imagesc(correctImage(N));set(gca,'FontSize',15);colorbar;
-    colormap(parula);axis off;title('0 deg'); caxis([0, 65535])
-    
-    subplot(2,2,2);
-    imagesc(correctImage(N+1));set(gca,'FontSize',15);colorbar;
-    colormap(parula);axis off;title('90 deg'); caxis([0, 65535])
-    
-    subplot(2,2,3);
-    imagesc(correctImage(N+2));set(gca,'FontSize',15);colorbar;
-    colormap(parula);axis off;title('45 deg'); caxis([0, 65535])
-    
-    subplot(2,2,4);
-    imagesc(correctImage(N+3));set(gca,'FontSize',15);colorbar;
-    colormap(parula);axis off;title('135 deg'); caxis([0, 65535])
-end
-
-figure(3)
-subplot(1,2,1);
-errorbar([0 delta], [D1 D2], stdevDOLP); title('DoLP vs Delta'); axis([0 delta, 0 100]);
-xlabel('Delta (deg)'); ylabel('DoLP');
-
-subplot(1,2,2);
-errorbar([0 delta], [A1 A2], stdevAOLP); title('AoLP vs Delta');
-xlabel('Delta (deg)'); ylabel('AoLP');
+ReadUV_Data(f, trueDel)
