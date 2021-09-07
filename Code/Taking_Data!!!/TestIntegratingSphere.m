b8c79c28ee88d833ed5774b0a7e62a98536b895b
AtkinHyatt <atkindavidhyatt@email.arizona.edu>
2021-09-07 09:06:49 -0700

    Allows for any number of data points to be collected

--- Code/Taking_Data!!!/TestIntegratingSphere.m
+++ Code/Taking_Data!!!/TestIntegratingSphere.m
@@ -6,6 +6,8 @@
 
 %% Initialize
 %initializeUV;
+saving_dir = 'C:\ULTRASIP_Data\August2021\Uncorrected Data\';
+
 stop(vid)
 expo = input('Exposure time in seconds? ');
 src.ExposureTime = expo;
@@ -14,56 +16,54 @@ if expo >= 5
    vid.Timeout = 2 * expo;
 end
 
-delta = input('Delta value in degrees? ');
+iter = input('How many data points? ');
 
 % Configure camera
 triggerconfig(vid, 'manual');
 
 %% Take data
 % Start camera comms
+stop(vid)
 start(vid);
 
 % initialize variables
 deg = [0, 45, 90, 135];
+delta = 180/(iter-1); lenDel = length(delta);
+h = home;
+
 L = length(deg);
-image = zeros(2*L,512,512);
+image = zeros(iter*L,512,512);
 
-% First data point
-for N = 1 : L
-    Move_motor(deg(N),ELL14);
-
-    pause(2)
+% Collect Data
+for ii = 1 : iter
+    fprintf("Data point %d\n", ii);
     
-    image(N,:,:) = UV_data(vid,framesPerTrigger) - dark;
-    disp('image taken')
+    % Scan
+    for N = 1 : L
+        Move_motor(deg(N),ELL14);
+
+        pause(2)
+    
+        image(N+4*ii-4,:,:) = UV_data(vid,framesPerTrigger) - dark;
+        disp('image taken')
+    end
+    
+    % "Tilt" instrument by delta degrees
+    if ~isempty(instrfind(ELL14,'Status','closed'))
+        fopen(ELL14);
+    end
+    
+    h = dec2hex(round(398.222222222222 * delta + hex2dec(h)), 8);
+    fprintf(ELL14, "%s", "0so" + h);
 end
 
-% "Tilt" instrument to delta
-fopen(ELL14);
-h = dec2hex(round(398.222222222222 * delta + hex2dec(home)), 8);
-trueDel = TranslateELL14(query(ELL14, "0go"));
-fprintf(ELL14, "%s", "0so" + h);
-fclose(ELL14);
-
-% Second data point
-for N = 1 : L
-    Move_motor(deg(N),ELL14);
-    
-    pause(2)
-    
-    image(N + L,:,:) = UV_data(vid,framesPerTrigger) - dark;
-    disp('image taken')
-end
-
-%Close instruments
+% Close instruments, reset home position
 stop(vid)
-fopen(ELL14);
 fprintf(ELL14, "%s", "0so" + home);
 fclose(ELL14);
 
 %% Save data
 % Define saving  directory 
-saving_dir = 'C:\ULTRASIP_Data\July2021\Uncorrected Data\';
 
 % Create data directory 
 date = datestr(now,'yyyy-mm-dd'); % get date
@@ -77,17 +77,10 @@ for N = 1 : length(ex)
        break
    end
 end
-
-del = char(string(delta));
-for N = 1 : length(del)
-   if del(N) == '.'
-       del(N) = '-';
-       break
-   end
-end
+num = int2str(iter);
 
 global filename; global file;
-file = [date '_' time '_' ex '_' del '.h5'];
+file = [date '_' time '_' ex '_' num '.h5'];
 filename = [saving_dir '' file];
 
 % Create h5
@@ -97,7 +90,7 @@ h5write(filename,'/measurement/images',image);
 % Write attibutes to directory
 h5writeatt(filename,'/measurement/images/','date', date);
 h5writeatt(filename,'/measurement/images/','time', time);
-h5writeatt(filename,'/measurement/images/','delta', trueDel);
+h5writeatt(filename,'/measurement/images/','datapoints', iter);
 h5writeatt(filename,'/measurement/images/','exposure', expo);
 
 %% Process data
@@ -105,4 +98,4 @@ addpath('C:\ULTRASIP\Code\Data_Analysis');
 ULTRASIP_IntegratingSphere_Analysis;
 
 %% Plot data
-ReadUV_Data(f, trueDel)
+ReadUV_Data(f)
