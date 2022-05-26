e0c89409fab4be4fce83398058c494c4f0261f07
cdeleon <cdeleon@email.arizona.edu>
2022-05-26 12:40:06 -0700

    modifications

--- Code/GRASP/GRASPinversion_CD.py
+++ Code/GRASP/GRASPinversion_CD.py
@@ -14,6 +14,26 @@ from matplotlib import pyplot as plt
 import numpy as np
 import datetime as dt
 
+wvStrFrmt =  '($%4.2f\\mu m$)' # Format of Y-axis labels 
+sza = 30 # solar zenith angle
+wvls = [0.470] # wavelengths in μm
+msTyp = [41, 42, 43] # grasp measurements types (I, Q, U) [must be in ascending order]
+azmthΑng = np.r_[0:190:10] # azimuth angles to simulate (0,10,...,175)
+vza = 180-np.r_[0:75:5] # viewing zenith angles to simulate [in GRASP cord. sys.]
+
+
+# prep geometry inputs for GRASP
+Nvza = len(vza)
+Nazimth = len(azmthΑng)
+thtv_g = np.tile(vza, len(msTyp)*len(azmthΑng))
+phi_g = np.tile(np.concatenate([np.repeat(φ, len(vza)) for φ in azmthΑng]), len(msTyp))
+nbvm = len(thtv_g)/len(msTyp)*np.ones(len(msTyp), int)
+meas = np.r_[np.repeat(0.1, nbvm[0]), np.repeat(0.01, nbvm[1]), np.repeat(0.01, nbvm[2])] # dummy values
+
+# define the "pixel" we want to simulate
+nowPix = pixel(dt.datetime.now(), 1, 1, 0, 0, masl=0, land_prct=100)
+for wvl in wvls: nowPix.addMeas(wvl, msTyp, nbvm, sza, thtv_g, phi_g, meas)
+
 #paths
 fwdModelYAMLpath = '/home/cdeleon/ULTRASIP/Code/GRASP/SettingFiles/settings_1lambda_airmspi_inversion.yml'
 binPathGRASP = '/home/cdeleon/grasp/build/bin/grasp'
