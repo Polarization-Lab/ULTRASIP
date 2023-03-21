5d4167c315454047f78759326bd70cf04598540a
ClarissaDeLeon <cdeleon@github.com>
2023-03-21 12:39:11 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -808,15 +808,7 @@ def main():  # Main code
         raz_555 = (saz - vaz_555);
         raz_660 = (saz - vaz_660);
         raz_865 = (saz - vaz_865);
-        
-        if(loop == mid_step):
-            raz_355 = 180-(saz - vaz_355);
-            raz_380 = 180-(saz - vaz_380);
-            raz_445 =  180-(saz - vaz_445);
-            raz_470 = 180-(saz - vaz_470);
-            raz_555 = 180-(saz - vaz_555);
-            raz_660 = 180-(saz - vaz_660);
-            raz_865 = (saz - vaz_865);
+
         
 
             
