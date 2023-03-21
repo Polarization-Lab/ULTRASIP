c9f847239ff13c9cd2875d42a1a7900ef8701b7b
ClarissaDeLeon <cdeleon@github.com>
2023-03-21 14:56:57 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -792,22 +792,22 @@ def main():  # Main code
         
 # Calculate the relative azimuth angle in the GRASP convention
         #Clarissa way
-        # raz_355 = 180-(saz - vaz_355);
-        # raz_380 = 180-(saz - vaz_380);
-        # raz_445 =  180-(saz - vaz_445);
-        # raz_470 = 180-(saz - vaz_470);
-        # raz_555 = 180-(saz - vaz_555);
-        # raz_660 = 180-(saz - vaz_660);
-        # raz_865 = 180-(saz - vaz_865);
+        raz_355 = 180-(saz - vaz_355);
+        raz_380 = 180-(saz - vaz_380);
+        raz_445 =  180-(saz - vaz_445);
+        raz_470 = 180-(saz - vaz_470);
+        raz_555 = 180-(saz - vaz_555);
+        raz_660 = 180-(saz - vaz_660);
+        raz_865 = 180-(saz - vaz_865);
         
-        # # if(loop == mid_step):
-        raz_355 = (saz - vaz_355);
-        raz_380 = (saz - vaz_380);
-        raz_445 =  (saz - vaz_445);
-        raz_470 = (saz - vaz_470);
-        raz_555 = (saz - vaz_555);
-        raz_660 = (saz - vaz_660);
-        raz_865 = (saz - vaz_865);
+        # # # if(loop == mid_step):
+        # raz_355 = (saz - vaz_355);
+        # raz_380 = (saz - vaz_380);
+        # raz_445 =  (saz - vaz_445);
+        # raz_470 = (saz - vaz_470);
+        # raz_555 = (saz - vaz_555);
+        # raz_660 = (saz - vaz_660);
+        # raz_865 = (saz - vaz_865);
 
         # #Clarissa way
         # raz_355 = (saz - vaz_355)
@@ -924,13 +924,13 @@ def main():  # Main code
         i_in_polar_median[loop,1] = eqr_i_660
         i_in_polar_median[loop,2] = eqr_i_865
         
-        q_median[loop,0] = eqr_qg_470
-        q_median[loop,1] = eqr_qg_660
-        q_median[loop,2] = eqr_qg_865
+        q_median[loop,0] = -eqr_qg_470
+        q_median[loop,1] = -eqr_qg_660
+        q_median[loop,2] = -eqr_qg_865
         
-        u_median[loop,0] = -eqr_ug_470
-        u_median[loop,1] = -eqr_ug_660
-        u_median[loop,2] = -eqr_ug_865
+        u_median[loop,0] = eqr_ug_470
+        u_median[loop,1] = eqr_ug_660
+        u_median[loop,2] = eqr_ug_865
           
         ipol_median[loop,0] = eqr_ipol_470
         ipol_median[loop,1] = eqr_ipol_660
