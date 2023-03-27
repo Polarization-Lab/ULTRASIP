91b3d81f9b2a88b88e28dbb6604d75e0c4ccbe74
ClarissaDeLeon <cdeleon@github.com>
2023-03-27 09:35:46 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -799,36 +799,36 @@ def main():  # Main code
 # Calculate the relative azimuth angle in the GRASP convention
         
         # # # # # if(loop == mid_step):
-        # raz_355 = (saz - vaz_355);
-        # raz_380 = (saz - vaz_380);
-        # raz_445 =  (saz - vaz_445);
-        # raz_470 = (saz - vaz_470);
-        # raz_555 = (saz - vaz_555);
-        # raz_660 = (saz - vaz_660);
-        # raz_865 = (saz - vaz_865);
+        raz_355 = (saz - vaz_355);
+        raz_380 = (saz - vaz_380);
+        raz_445 =  (saz - vaz_445);
+        raz_470 = (saz - vaz_470);
+        raz_555 = (saz - vaz_555);
+        raz_660 = (saz - vaz_660);
+        raz_865 = (saz - vaz_865);
         
-        # if raz_355 < 0.0:
-        #     raz_355 = raz_355 + 360
-        # if raz_380 < 0.0:
-        #     raz_380 = raz_380 + 360
-        # if raz_445 < 0.0:
-        #     raz_445 = raz_445 + 360
-        # if raz_470 < 0.0:
-        #     raz_470 = raz_470 + 360
-        # if raz_555 < 0.0:
-        #     raz_555 = raz_555 + 360
-        # if raz_660 < 0.0:
-        #     raz_660 = raz_660 + 360
-        # if raz_865 < 0.0:
-        #     raz_865 = raz_865 + 360
+        if raz_355 < 0.0:
+            raz_355 = raz_355 + 360
+        if raz_380 < 0.0:
+            raz_380 = raz_380 + 360
+        if raz_445 < 0.0:
+            raz_445 = raz_445 + 360
+        if raz_470 < 0.0:
+            raz_470 = raz_470 + 360
+        if raz_555 < 0.0:
+            raz_555 = raz_555 + 360
+        if raz_660 < 0.0:
+            raz_660 = raz_660 + 360
+        if raz_865 < 0.0:
+            raz_865 = raz_865 + 360
 
-        raz_355 = 180-(saz - vaz_355);
-        raz_380 = 180-(saz - vaz_380);
-        raz_445 = 180-(saz - vaz_445);
-        raz_470 = 180-(saz - vaz_470);
-        raz_555 = 180-(saz - vaz_555);
-        raz_660 = 180-(saz - vaz_660);
-        raz_865 = 180-(saz - vaz_865);
+        # raz_355 = 180-(saz - vaz_355);
+        # raz_380 = 180-(saz - vaz_380);
+        # raz_445 = 180-(saz - vaz_445);
+        # raz_470 = 180-(saz - vaz_470);
+        # raz_555 = 180-(saz - vaz_555);
+        # raz_660 = 180-(saz - vaz_660);
+        # raz_865 = 180-(saz - vaz_865);
 
         
 
@@ -917,13 +917,13 @@ def main():  # Main code
         i_in_polar_median[loop,1] = eqr_i_660
         i_in_polar_median[loop,2] = eqr_i_865
         
-        q_median[loop,0] = eqr_qg_470
-        q_median[loop,1] = eqr_qg_660
-        q_median[loop,2] = eqr_qg_865
+        q_median[loop,0] = -eqr_qg_470
+        q_median[loop,1] = -eqr_qg_660
+        q_median[loop,2] = -eqr_qg_865
         
-        u_median[loop,0] = -eqr_ug_470
-        u_median[loop,1] = -eqr_ug_660
-        u_median[loop,2] = -eqr_ug_865
+        u_median[loop,0] = eqr_ug_470
+        u_median[loop,1] = eqr_ug_660
+        u_median[loop,2] = eqr_ug_865
           
         ipol_median[loop,0] = eqr_ipol_470
         ipol_median[loop,1] = eqr_ipol_660
@@ -949,7 +949,7 @@ def main():  # Main code
 # Generate the base output file name
     #outfile_base = "AirMSPI_"+this_date_str+"_"+this_time_str+"_"
     #outfile_base = outfile_base+this_target_str+"_"
-    outfile_base = 'RotfromScat_180raznegQ'
+    outfile_base = 'RotfromScat_360raznegU'
 
 # Get the software version number to help track issues
     hold = os.path.basename(__file__)
