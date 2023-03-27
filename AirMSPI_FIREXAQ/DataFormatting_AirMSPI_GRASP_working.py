a5e4ea1712682174685fbae9f96f51da957ff85d
ClarissaDeLeon <cdeleon@github.com>
2023-03-27 09:48:11 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -791,9 +791,9 @@ def main():  # Main code
         # rotmatrix8 = np.array([[np.cos(2*alpha8),-np.sin(2*alpha8)],[np.sin(2*alpha8),np.cos(2*alpha8)]]); 
         # qg_865, ug_865 = -stokesin8s #rotmatrix8@stokesin8s
         
-        qg_470, ug_470 = -stokesin4
-        qg_660, ug_660 = -stokesin6
-        qg_865, ug_865 = -stokesin8
+        qg_470, ug_470 = -stokesin4s
+        qg_660, ug_660 = -stokesin6s
+        qg_865, ug_865 = -stokesin8s
     
         
 # Calculate the relative azimuth angle in the GRASP convention
@@ -808,19 +808,19 @@ def main():  # Main code
         raz_865 = (saz - vaz_865);
         
         if raz_355 < 0.0:
-            raz_355 = raz_355 + 180
+            raz_355 = raz_355 + 360
         if raz_380 < 0.0:
-            raz_380 = raz_380 + 180
+            raz_380 = raz_380 + 360
         if raz_445 < 0.0:
-            raz_445 = raz_445 + 180
+            raz_445 = raz_445 + 360
         if raz_470 < 0.0:
-            raz_470 = raz_470 + 180
+            raz_470 = raz_470 + 360
         if raz_555 < 0.0:
-            raz_555 = raz_555 + 180
+            raz_555 = raz_555 + 360
         if raz_660 < 0.0:
-            raz_660 = raz_660 + 180
+            raz_660 = raz_660 + 360
         if raz_865 < 0.0:
-            raz_865 = raz_865 + 180
+            raz_865 = raz_865 + 360
 
         # raz_355 = 180-(saz - vaz_355);
         # raz_380 = 180-(saz - vaz_380);
@@ -949,7 +949,7 @@ def main():  # Main code
 # Generate the base output file name
     #outfile_base = "AirMSPI_"+this_date_str+"_"+this_time_str+"_"
     #outfile_base = outfile_base+this_target_str+"_"
-    outfile_base = 'RotfromScat_180negonlyraznegU'
+    outfile_base = 'RotfromScat_360raznegU'
 
 # Get the software version number to help track issues
     hold = os.path.basename(__file__)
