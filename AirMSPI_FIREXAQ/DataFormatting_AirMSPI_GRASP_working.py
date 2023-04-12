9adddeeeadd0e58f143129a8bd20865b652ed0fb
ClarissaDeLeon <cdeleon@github.com>
2023-04-12 13:03:01 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -799,13 +799,13 @@ def main():  # Main code
 # Calculate the relative azimuth angle in the GRASP convention
         
         # # # # # if(loop == mid_step):
-        raz_355 = -(saz - vaz_355);
-        raz_380 = -(saz - vaz_380);
-        raz_445 =  -(saz - vaz_445);
-        raz_470 = -(saz - vaz_470);
-        raz_555 = -(saz - vaz_555);
-        raz_660 = -(saz - vaz_660);
-        raz_865 = -(saz - vaz_865);
+        raz_355 = (vaz_355-saz);
+        raz_380 = (vaz_380-saz);
+        raz_445 = (vaz_445-saz);
+        raz_470 = (vaz_470-saz);
+        raz_555 = (vaz_555-saz);
+        raz_660 = (vaz_660-saz);
+        raz_865 = (vaz_865-saz);
         
         if raz_355 < 0.0:
             raz_355 = raz_355 + 360
