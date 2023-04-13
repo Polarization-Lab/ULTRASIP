f93955c1fa73af02ac0048dc3a6fbf29ec23c400
ClarissaDeLeon <cdeleon@github.com>
2023-04-13 12:23:31 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -829,40 +829,40 @@ def main():  # Main code
         # if raz_865 < 0.0:
         #     raz_865 = raz_865 + 360
 
-        # raz_355 = (saz - vaz_355);
-        # raz_380 = (saz - vaz_380);
-        # raz_445 = (saz - vaz_445);
-        # raz_470 = (saz - vaz_470);
-        # raz_555 = (saz - vaz_555);
-        # raz_660 = (saz - vaz_660);
-        # raz_865 = (saz - vaz_865);
+        raz_355 = (saz - vaz_355)+180;
+        raz_380 = (saz - vaz_380)+180;
+        raz_445 = (saz - vaz_445)+180;
+        raz_470 = (saz - vaz_470)+180;
+        raz_555 = (saz - vaz_555)+180;
+        raz_660 = (saz - vaz_660)+180;
+        raz_865 = (saz - vaz_865)+180;
         
-        raz_355 = -(vaz_355-saz);
-        raz_380 = -(vaz_380-saz);
-        raz_445 = -(vaz_445-saz);
-        raz_470 = -(vaz_470-saz);
-        raz_555 = -(vaz_555-saz);
-        raz_660 = -(vaz_660-saz);
-        raz_865 = -(vaz_865-saz);
+        # raz_355 = -(vaz_355-saz);
+        # raz_380 = -(vaz_380-saz);
+        # raz_445 = -(vaz_445-saz);
+        # raz_470 = -(vaz_470-saz);
+        # raz_555 = -(vaz_555-saz);
+        # raz_660 = -(vaz_660-saz);
+        # raz_865 = -(vaz_865-saz);
 
-        if raz_355 < 0.0:
-            raz_355 = raz_355 + 360
-        if raz_380 < 0.0:
-            raz_380 = raz_380 + 360
-        if raz_445 < 0.0:
-            raz_445 = raz_445 + 360
-        if raz_470 < 0.0:
-            raz_470 = raz_470 + 360
-        if raz_555 < 0.0:
-            raz_555 = raz_555 + 360
-        if raz_660 < 0.0:
-            raz_660 = raz_660 + 360
-        if raz_865 < 0.0:
-            raz_865 = raz_865 + 360
+        # if raz_355 < 0.0:
+        #     raz_355 = raz_355 + 360
+        # if raz_380 < 0.0:
+        #     raz_380 = raz_380 + 360
+        # if raz_445 < 0.0:
+        #     raz_445 = raz_445 + 360
+        # if raz_470 < 0.0:
+        #     raz_470 = raz_470 + 360
+        # if raz_555 < 0.0:
+        #     raz_555 = raz_555 + 360
+        # if raz_660 < 0.0:
+        #     raz_660 = raz_660 + 360
+        # if raz_865 < 0.0:
+        #     raz_865 = raz_865 + 360
         
 
 
-        print(scat_355,scat_380,scat_445,scat_470,scat_555,scat_660,scat_865)
+        print(vaz_355,vaz_380,vaz_445,vaz_470,vaz_555,vaz_660,vaz_865,saz)
             
         
 
