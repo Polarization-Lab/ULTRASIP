2ef9c63b6cd970a79c1ca14fefe652cfc4d23984
ClarissaDeLeon <cdeleon@github.com>
2023-05-10 14:42:14 -0700

    message

--- AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
+++ AirMSPI_FIREXAQ/DataFormatting_AirMSPI_GRASP_working.py
@@ -706,9 +706,9 @@ def main():  # Main code
         k_8 = np.array([np.cos(np.radians(vaz_865))*np.sin(np.radians(vza_865)), -np.sin(np.radians(vaz_865))*np.sin(np.radians(vza_865)), np.cos(np.radians(vza_865))]);
 
   
-        qg_470, ug_470 = stokesin4s
-        qg_660, ug_660 = stokesin6s
-        qg_865, ug_865 = stokesin8s
+        qg_470, ug_470 = stokesin4
+        qg_660, ug_660 = stokesin6
+        qg_865, ug_865 = stokesin8
     
         
 # Calculate the relative azimuth angle in the GRASP convention
@@ -856,13 +856,13 @@ def main():  # Main code
         i_in_polar_mean[loop,1] = eqr_i_660
         i_in_polar_mean[loop,2] = eqr_i_865
         
-        q_mean[loop,0] = -eqr_qg_470
-        q_mean[loop,1] = -eqr_qg_660
-        q_mean[loop,2] = -eqr_qg_865
+        q_mean[loop,0] = eqr_qg_470
+        q_mean[loop,1] = eqr_qg_660
+        q_mean[loop,2] = eqr_qg_865
         
-        u_mean[loop,0] = -eqr_ug_470
-        u_mean[loop,1] = -eqr_ug_660
-        u_mean[loop,2] = -eqr_ug_865
+        u_mean[loop,0] = eqr_ug_470
+        u_mean[loop,1] = eqr_ug_660
+        u_mean[loop,2] = eqr_ug_865
           
         ipol_mean[loop,0] = eqr_ipol_470
         ipol_mean[loop,1] = eqr_ipol_660
@@ -888,7 +888,7 @@ def main():  # Main code
 # Generate the base output file name
     #outfile_base = "AirMSPI_"+this_date_str+"_"+this_time_str+"_"
     #outfile_base = outfile_base+this_target_str+"_"
-    outfile_base = 'RotfromScat'
+    outfile_base = 'RotfromMerd'
 
 # Get the software version number to help track issues
     hold = os.path.basename(__file__)
