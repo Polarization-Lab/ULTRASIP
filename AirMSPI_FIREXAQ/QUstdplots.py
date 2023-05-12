be638d6f2996e46c5b038fee8974f1908194dcaf
ClarissaDeLeon <cdeleon@github.com>
2023-05-11 17:07:16 -0700

    message

--- AirMSPI_FIREXAQ/QUstdplots.py
+++ AirMSPI_FIREXAQ/QUstdplots.py
@@ -455,8 +455,8 @@ if __name__ == '__main__':
 #       figpath is where the image output should be stored
 
 
-    basepath = "C:/Users/ULTRASIP_1/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1023/Bakersfield"
-    figpath = "C:/Users/ULTRASIP_1/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1023/Bakersfield/Plots"
+    basepath = "C:/Users/ULTRASIP_1/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1123/2FIREX"
+    figpath = "C:/Users/ULTRASIP_1/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1123/2FIREX/Plots"
 
     # basepath = "C:/Users/Clarissa/Documents/GitHub/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1023/1FIREXR2"
     # figpath = "C:/Users/Clarissa/Documents/GitHub/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/May1023/1FIREXR2/Plots"
@@ -1166,178 +1166,104 @@ if __name__ == '__main__':
 
     os.chdir(figpath)
     
-    fig, (ax1,ax2,ax3) = plt.subplots(
-         nrows=3, ncols=1, dpi=240,figsize=(6, 8))
-    
-    # ax1.scatter(scat[:,0],i_obs[:,0],marker='x',color="green",label="355nm")
-    # ax1.scatter(scat[:,1],i_obs[:,1],marker='x',color="pink",label="380nm")
-    # ax1.scatter(scat[:,2],i_obs[:,2],marker='x',color="brown",label="445nm")  
-    # ax1.scatter(scat[:,4],i_obs[:,4],marker='x',color="purple",label="555nm")
-    
-    ax1.plot(scat[:,0],i_obs[:,0],linestyle='dashed',color="green",label="355nm")
-    ax1.plot(scat[:,1],i_obs[:,1],linestyle='dashed',color="pink",label="380nm")
-    ax1.plot(scat[:,2],i_obs[:,2],linestyle='dashed',color="brown",label="445nm")  
-    ax1.plot(scat[:,4],i_obs[:,4],linestyle='dashed',color="purple",label="555nm")
-    
-
-    ax1.plot(scat[:,0],i_mod[:,0],color="green")
-    ax1.plot(scat[:,1],i_mod[:,1],color="pink")
-    ax1.plot(scat[:,2],i_mod[:,2],color="brown")
-    ax1.plot(scat[:,4],i_mod[:,4],color="purple")
-
-    # ax1.scatter(scat[:,3],i_obs[:,3],marker='x',color="blue",label="470nm")
-    # ax1.scatter(scat[:,5],i_obs[:,5],marker='x',color="red",label="660nm")
-    # ax1.scatter(scat[:,6],i_obs[:,6],marker='x',color="orange",label="865nm")
-    
-    ax1.plot(scat[:,3],i_obs[:,3],linestyle='dashed',color="blue",label="470nm")
-    ax1.plot(scat[:,5],i_obs[:,5],linestyle='dashed',color="red",label="660nm")
-    ax1.plot(scat[:,6],i_obs[:,6],linestyle='dashed',color="orange",label="865nm")
-
-    ax1.plot(scat[:,3],i_mod[:,3],color="blue")
-    ax1.plot(scat[:,5],i_mod[:,5],color="red")
-    ax1.plot(scat[:,6],i_mod[:,6],color="orange")
-    
-    # ymin = 0;
-    # ymax = 0.4;
-    # yticks = 0.1;
-     
-    #ax1.set_ylim(ymin,ymax)
-    #ax1.set_yticks(np.arange(ymin,ymax,yticks))
-    ax1.get_yaxis().get_major_formatter().labelOnlyBase = True
-    ax1.set_yscale("log")
-    # ax1.set_ylim(5e-2,2.5e-1)
-    # ax1.set_yticks(np.arange(0.03,0.3,0.06))
-    # ax1.set_ylabel('BRF(I)', fontsize='xx-large')
-    ax1.yaxis.set_label_coords(-.15, .5)
-    ax1.xaxis.set_label_coords(.5, -.1)
-    ax1.set_xticks([])
-    
-    # ax1.legend(loc='best',ncol=4,fontsize='medium')  # Upper right
-    # ax1.legend(loc='upper center', bbox_to_anchor=(0.5,3),
-    #       ncol=3, fancybox=True, shadow=True,fontsize='large')
-    
-    ax2.plot(scat[:,3],q_obs[:,0],linestyle='dashed',color="blue",label="470nm")
-    ax2.plot(scat[:,5],q_obs[:,1],linestyle='dashed',color="red",label="660nm")
-    ax2.plot(scat[:,6],q_obs[:,2],linestyle='dashed',color="orange",label="865nm")
-    
-    ax2.plot(scat[:,3],q_mod[:,0],color="blue")
-    ax2.plot(scat[:,5],q_mod[:,1],color="red")
-    ax2.plot(scat[:,6],q_mod[:,2],color="orange")
-         
-    ymin = 0.0;
-    ymax = 0.5;
-    yticks = 0.1;
-     
-    ax2.set_ylim(ymin,ymax)
-    ax2.set_yticks(np.arange(ymin,ymax,yticks))
-    # ax2.set_ylabel('BRF(Q)', fontsize='xx-large')
-    ax2.yaxis.set_label_coords(-.15, .5)
-    ax2.xaxis.set_label_coords(.5, -.1)
-    ax2.set_xticks([])
-    
-    # ax3.scatter(scat[:,3],u_obs[:,0],marker='x',color="blue",label="470nm")
-    # ax3.scatter(scat[:,5],u_obs[:,1],marker='x',color="red",label="660nm")
-    # ax3.scatter(scat[:,6],u_obs[:,2],marker='x',color="orange",label="865nm")
-    
-    ax3.plot(scat[:,3],u_obs[:,0],linestyle='dashed',color="blue",label="470nm")
-    ax3.plot(scat[:,5],u_obs[:,1],linestyle='dashed',color="red",label="660nm")
-    ax3.plot(scat[:,6],u_obs[:,2],linestyle='dashed',color="orange",label="865nm")
-    
-    ax3.plot(scat[:,3],u_mod[:,0],color="blue")
-    ax3.plot(scat[:,5],u_mod[:,1],color="red")
-    ax3.plot(scat[:,6],u_mod[:,2],color="orange")
-         
-    ymin = -0.06;
-    ymax =0.01;
-    yticks = 0.02;
-     
-    ax3.set_ylim(ymin,ymax)
-    ax3.set_yticks(np.arange(ymin,ymax,yticks))
-    # ax3.set_ylabel('BRF(U)', fontsize='xx-large')
-    
-    # ax3.set_xlim(60,145)
-    # ax3.set_xticks(np.arange(60,145,10))
-    ax3.set_xlim(65,150)
-    ax3.set_xticks(np.arange(65,150,10))
-    ax3.set_xlabel( u"\u03A9 [\u00b0]",fontsize='xx-large')
-    
-    ax3.yaxis.set_label_coords(-.15, .5)
-    ax3.xaxis.set_label_coords(.5, -.1)
-   
-    plt.tight_layout()  
-    plt.show() 
     
     fig, ax = plt.subplots(3, 3,
                            sharex=True, 
-                           sharey='row')
+                           sharey=False,figsize=(18, 10), dpi=560)
 
-    ax[0,0].plot(scat[:,0],i_obs[:,0],linestyle='dashed',color="green")
-    ax[0,0].plot(scat[:,1],i_obs[:,1],linestyle='dashed',color="pink")
-    ax[0,0].plot(scat[:,2],i_obs[:,2],linestyle='dashed',color="brown")  
-    ax[0,0].plot(scat[:,4],i_obs[:,4],linestyle='dashed',color="purple")
+    ax[0,0].plot(scat[:,0],i_obs[:,0],linestyle='dashed',color="green",linewidth='3')
+    ax[0,0].plot(scat[:,1],i_obs[:,1],linestyle='dashed',color="pink",linewidth='3')
+    ax[0,0].plot(scat[:,2],i_obs[:,2],linestyle='dashed',color="brown",linewidth='3')  
+    ax[0,0].plot(scat[:,4],i_obs[:,4],linestyle='dashed',color="purple",linewidth='3')
     
     ax[0,0].set_ylabel('BRF(I)', fontsize='x-large')
-    ax[0,0].yaxis.set_label_coords(-.4, .5)
-    
-    ax[0,0].plot(scat[:,0],i_mod[:,0],color="green",label="355")
-    ax[0,0].plot(scat[:,1],i_mod[:,1],color="pink",label="380")
-    ax[0,0].plot(scat[:,2],i_mod[:,2],color="brown",label="445")
-    ax[0,0].plot(scat[:,4],i_mod[:,4],color="purple",label="555")
-    
-    ax[0,0].set_yticks([0.06,0.1,0.2])
-    
-    ax[0,1].plot(scat[:,3],i_obs[:,3],linestyle='dashed',color="blue")
+    ax[0,0].yaxis.set_label_coords(-.2, .5)
 
-    ax[0,1].plot(scat[:,5],i_obs[:,5],linestyle='dashed',color="red")
-    ax[0,1].plot(scat[:,6],i_obs[:,6],linestyle='dashed',color="orange")
-
-    ax[0,1].plot(scat[:,3],i_mod[:,3],color="blue",label='470')
-    ax[0,1].plot(scat[:,5],i_mod[:,5],color="red",label='660')
-    ax[0,1].plot(scat[:,6],i_mod[:,6],color="orange",label='865')
     
-    ax[0,0].legend(bbox_to_anchor=(3.62,1),
-          ncol=2,fontsize='medium',frameon=False)
-    ax[0,1].legend(bbox_to_anchor=(1.08,0.5),
-          ncol=2,fontsize='medium',frameon=False)
+    ax[0,0].plot(scat[:,0],i_mod[:,0],color="green",label="355",linewidth='3')
+    ax[0,0].plot(scat[:,1],i_mod[:,1],color="pink",label="380",linewidth='3')
+    ax[0,0].plot(scat[:,2],i_mod[:,2],color="brown",label="445",linewidth='3')
+    ax[0,0].plot(scat[:,4],i_mod[:,4],color="purple",label="555",linewidth='3')
+    
+    ax[0,0].set_yticks([0.04,0.06,0.1,0.15,0.2])
+    ax[0,0].yaxis.set_tick_params(labelsize=15)
+    
+    ax[0,1].plot(scat[:,3],i_obs[:,3],linestyle='dashed',color="blue",linewidth='3')
+    ax[0,1].plot(scat[:,5],i_obs[:,5],linestyle='dashed',color="red",linewidth='3')
+    ax[0,1].plot(scat[:,6],i_obs[:,6],linestyle='dashed',color="orange",linewidth='3')
+    ax[0,1].plot(scat[:,3],i_mod[:,3],color="blue",label='470',linewidth='3')
+    ax[0,1].plot(scat[:,5],i_mod[:,5],color="red",label='660',linewidth='3')
+    ax[0,1].plot(scat[:,6],i_mod[:,6],color="orange",label='865',linewidth='3')
+    
+    ax[0,1].set_yticks([0.02,0.04,0.06,0.08,0.1])
+    ax[0,1].yaxis.set_tick_params(labelsize=15)
+    
+    ax[0,0].legend(bbox_to_anchor=(3.25,0.80),
+          ncol=2,fontsize='xx-large',frameon=False)
+    ax[0,1].legend(bbox_to_anchor=(1.28,0.5),
+          ncol=2,fontsize='xx-large',frameon=False)
+    
     
     ax[1,0].set_ylabel('BRF(Q)', fontsize='x-large')
-    ax[1,0].yaxis.set_label_coords(-.4, .5)
+    ax[1,0].yaxis.set_label_coords(-.2, .5)
     
-    ax[1,0].plot(scat[:,3],q_obs[:,0],linestyle='dashed',color="blue",label="470nm")
-    ax[1,0].fill_between(scat[:,3], (q_obs[:,0]-qs[:,0]),(q_obs[:,0]-qs[:,0]), color="blue",alpha=0.4)
-
-
-    ax[1,1].plot(scat[:,5],q_obs[:,1],linestyle='dashed',color="red",label="660nm")
-    ax[1,1].fill_between(scat[:,5], (q_obs[:,1]-qs[:,1]),(q_obs[:,1]-qs[:,1]),color="red",alpha=0.4)
-
-
-    ax[1,2].plot(scat[:,6],q_obs[:,2],linestyle='dashed',color="orange",label="865nm")
-    ax[1,2].fill_between(scat[:,6], (q_obs[:,2]-qs[:,2]),(q_obs[:,2]-qs[:,2]),color="orange",alpha=0.4)
     
-    ax[1,0].plot(scat[:,3],q_mod[:,0],color="blue")
-    ax[1,1].plot(scat[:,5],q_mod[:,1],color="red")
-    ax[1,2].plot(scat[:,6],q_mod[:,2],color="orange")
+    ax[1,0].plot(scat[:,3],q_obs[:,0],linestyle='dashed',color="blue",label="470nm",linewidth='3')
+    ax[1,0].errorbar(scat[:,3],q_obs[:,0],qs[:,0], marker = 'd',alpha=0.4,color='blue',markersize=15)
+    
+    ax[1,0].set_yticks([0.30,0.35,0.40,0.45,0.50])
+    ax[1,0].yaxis.set_tick_params(labelsize=15)
+
+    ax[1,1].plot(scat[:,5],q_obs[:,1],linestyle='dashed',color="red",label="660nm",linewidth='3')
+    ax[1,1].errorbar(scat[:,5],q_obs[:,1],qs[:,1],marker = 'd',alpha=0.4,color='red',markersize=15)
+    
+    ax[1,1].set_yticks([0.15,0.25,0.30,0.35,0.40])
+    ax[1,1].yaxis.set_tick_params(labelsize=15)
+
+    ax[1,2].plot(scat[:,6],q_obs[:,2],linestyle='dashed',color="orange",label="865nm",linewidth='3')
+    ax[1,2].errorbar(scat[:,6],q_obs[:,2],qs[:,2], marker = 'd',alpha=0.6,color='orange',markersize=15)
+    
+    ax[1,2].set_yticks([0.05,0.1,0.13,0.15,0.35])
+    ax[1,2].yaxis.set_tick_params(labelsize=15)
+
+    
+    ax[1,0].plot(scat[:,3],q_mod[:,0],color="blue",linewidth='3')
+    ax[1,1].plot(scat[:,5],q_mod[:,1],color="red",linewidth='3')
+    ax[1,2].plot(scat[:,6],q_mod[:,2],color="orange",linewidth='3')
     
     fig.delaxes(ax[0,2])
     
     ax[2,0].set_ylabel('BRF(U)', fontsize='x-large')
-    ax[2,0].yaxis.set_label_coords(-.4, .5)
+    ax[2,0].yaxis.set_label_coords(-0.2, .5)
     
-    ax[2,0].plot(scat[:,3],u_obs[:,0],linestyle='dashed',color="blue",label="470nm")
-    ax[2,0].fill_between(np.sort(scat[:,3]), (u_obs[:,0]-us[:,0]),(u_obs[:,0]-us[:,0]),color="blue",alpha=0.4)
+    ax[2,0].plot(scat[:,3],u_obs[:,0],linestyle='dashed',color="blue",label="470nm",linewidth='3')
+    ax[2,0].errorbar(scat[:,3],u_obs[:,0],us[:,0], marker = 'd',alpha=0.4,color='blue',markersize=15)
+    
+    ax[2,0].set_yticks([-0.06,-0.04,-0.02,0,0.1])
+    ax[2,0].yaxis.set_tick_params(labelsize=15)
 
-    ax[2,1].plot(scat[:,5],u_obs[:,1],linestyle='dashed',color="red",label="660nm")
-    ax[2,1].fill_between(scat[:,5], (u_obs[:,1]-us[:,1]),(u_obs[:,1]-us[:,1]),color="red",alpha=0.4)
+    ax[2,1].plot(scat[:,5],u_obs[:,1],linestyle='dashed',color="red",label="660nm",linewidth='3')
+    ax[2,1].errorbar(scat[:,5],u_obs[:,1],us[:,1],marker = 'd',alpha=0.4,color='red',markersize=15)
+    
+    ax[2,1].set_yticks([-0.04,-0.03,-0.02,-0.01,0])
+    ax[2,1].yaxis.set_tick_params(labelsize=15)
 
-    ax[2,2].plot(scat[:,6],u_obs[:,2],linestyle='dashed',color="orange",label="865nm")
-    ax[2,2].fill_between(scat[:,6], u_obs[:,2]-np.std(u_obs[:,2]), u_obs[:,2]+np.std(u_obs[:,2]),color="orange",alpha=0.4)
+    ax[2,2].plot(scat[:,6],u_obs[:,2],linestyle='dashed',color="orange",label="865nm",linewidth='3')
+    ax[2,2].errorbar(scat[:,6],u_obs[:,2],us[:,2], marker = 'd',alpha=0.6,color='orange',markersize=15)
+    
+    ax[2,2].set_yticks([-0.01,-0.008,-0.004,0,0.003])
+    ax[2,2].yaxis.set_tick_params(labelsize=15)
 
-    ax[2,0].plot(scat[:,3],u_mod[:,0],color="blue")
-    ax[2,1].plot(scat[:,5],u_mod[:,1],color="red")
-    ax[2,2].plot(scat[:,6],u_mod[:,2],color="orange")
+
+    ax[2,0].plot(scat[:,3],u_mod[:,0],color="blue",linewidth='3')
+    ax[2,1].plot(scat[:,5],u_mod[:,1],color="red",linewidth='3')
+    ax[2,2].plot(scat[:,6],u_mod[:,2],color="orange",linewidth='3')
     
 
     ax[2,1].set_xlabel(u"\u03A9 [\u00b0]",fontsize='xx-large')   
+    ax[2,1].xaxis.set_tick_params(labelsize=15)
+    ax[2,2].xaxis.set_tick_params(labelsize=15)
+    ax[2,0].xaxis.set_tick_params(labelsize=15)
     
     plt.show()
     
