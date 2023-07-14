ddf03fee6a414b6fd7fef4d4a3edc4606f72c65b
ClarissaDeLeon <cdeleon@github.com>
2023-07-14 11:54:20 -0700

    new sdat

--- AirMSPI_FIREXAQ/sdata_FWD.py
+++ AirMSPI_FIREXAQ/sdata_FWD.py
@@ -181,7 +181,7 @@ for i in range(len(content)):
         vza9 = ' ' + content[i+9].split()[2]
         
         vza = vza1+vza2+vza3+vza4+vza5+vza6+vza7+vza8+vza9
-        vza = vza*9
+        vza = vza*18
         
 out_str = out_str+' '+vza
 
@@ -199,7 +199,7 @@ for i in range(len(content)):
         raz9 = ' ' + content[i+9].split()[3]
         
         raz = raz1+raz2+raz3+raz4+raz5+raz6+raz7+raz8+raz9
-        raz = raz*9
+        raz = raz*18
         
 out_str = out_str+' '+raz
         
