a0b01bd05a4801d147cc349ba37df2f99caf8bf9
ClarissaDeLeon <cdeleon@github.com>
2023-07-17 16:47:21 -0700

    new output

--- AirMSPI_FIREXAQ/geolaction_working.py
+++ AirMSPI_FIREXAQ/geolaction_working.py
@@ -36,10 +36,10 @@ def image_crop(a):
 
         mid_row = a.shape[0] // 2
         mid_col = a.shape[1] // 2
-        start_row = mid_row - 524
-        end_row = mid_row + 524
-        start_col = mid_col - 524
-        end_col = mid_col + 524
+        start_row = mid_row - 1048
+        end_row = mid_row + 1048
+        start_col = mid_col - 1048
+        end_col = mid_col + 1048
         
         a = a[start_row:end_row, start_col:end_col]
         
@@ -48,10 +48,10 @@ def image_crop(a):
 #_______________Start the main code____________#
 def main():  # Main code
     # Change directory to the datapath
-    datapath = "C:/Users/Clarissa/Documents/AirMSPI/Washington"
+    datapath = "C:/Users/ULTRASIP_1/Documents/Inchelium"
     os.chdir(datapath)
     # Open the HDF file
-    file = h5py.File('AirMSPI_ER2_GRP_TERRAIN_20190807_182404Z_WA-Inchelium_645F_F01_V006.hdf', 'r')
+    file = h5py.File('AirMSPI_ER2_GRP_TERRAIN_20190807_202556Z_WA-Inchelium_571F_F01_V006.hdf', 'r')
 
     # Read the attribute 'I' from the HDF file
     attribute_data = file['/HDFEOS/GRIDS/470nm_band/Data Fields/I/'][:]
@@ -64,27 +64,6 @@ def main():  # Main code
     latitude_data = image_crop(latitude_data)
     longitude_data = image_crop(longitude_data)
     
-    # Prompt the user to enter the center latitude and longitude
-    center_lat = float(input("Enter the center latitude: "))
-    center_lon = float(input("Enter the center longitude: "))
-
-    # Specify the size of the region of interest (ROI)
-    roi_size = 50  # Size of the ROI (10 by 10)
-
-    # Calculate the index ranges for the ROI
-    lat_start = np.abs(latitude_data - center_lat).argmin() - roi_size // 2
-    lat_end = lat_start + roi_size
-    lon_start = np.abs(longitude_data - center_lon).argmin() - roi_size // 2
-    lon_end = lon_start + roi_size
-    
-    # Extract the ROI data
-    roi_attribute = attribute_data[lat_start:lat_end, lon_start:lon_end]
-    roi_latitude = latitude_data[lat_start:lat_end]
-    roi_longitude = longitude_data[lon_start:lon_end]
-    
-    # Close the HDF file
-    file.close()
-
     # Plot the attribute 'I' over geolocation
     plt.figure(figsize=(10, 8))
     plt.scatter(longitude_data, latitude_data, c=attribute_data, cmap='gray')
@@ -94,18 +73,40 @@ def main():  # Main code
     plt.title('Attribute I over Geolocation')
     plt.show()
     
+    # Prompt the user to enter the center latitude and longitude
+    center_lat = float(input("Enter the center latitude: "))
+    center_lon = float(input("Enter the center longitude: "))
+
+    # Specify the size of the region of interest (ROI)
+    roi_size = 2  # Size of the ROI (10 by 10)
+    latitude_data = np.round(latitude_data,decimals=3)
+    longitude_data = np.round(longitude_data,decimals=3)
+    
+    # Calculate the index ranges for the ROI
+    lat_start,lon_start= np.where((latitude_data == center_lat) & (longitude_data == center_lon))
+
+    roi_attribute = attribute_data[lat_start,lon_start]
+    
+    # Extract the ROI data
+    roi_latitude = latitude_data[lat_start,lon_start]
+    roi_longitude = longitude_data[lat_start,lon_start]
+    
+    # Close the HDF file
+    file.close()
+
+    
     # Plot the attribute 'I' over the ROI
     plt.figure(figsize=(10, 8))
-    plt.scatter(roi_longitude, roi_latitude, c=roi_attribute, cmap='jet')
+    plt.scatter(roi_longitude, roi_latitude, c=roi_attribute, cmap='gray')
     plt.colorbar(label='Attribute I')
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Attribute I over ROI')
     plt.show()
-    
+
     return roi_attribute
     
 ### END MAIN FUNCTION
 if __name__ == '__main__':
-    main() 
+   roi= main() 
      
