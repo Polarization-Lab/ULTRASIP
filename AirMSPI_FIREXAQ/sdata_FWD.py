23072f140248ac9510b6e8c6da4dcbfd859568e2
Clarissa <cdeleon@email.arizona.edu>
2023-07-30 20:59:49 -0700

    m

--- AirMSPI_FIREXAQ/sdata_FWD.py
+++ AirMSPI_FIREXAQ/sdata_FWD.py
@@ -228,7 +228,7 @@ out_str = out_str+' '+sza*7
 #         vza = vza1+vza2+vza3+vza4+vza5 #+vza6+vza7+vza8+vza9
 #         vza = vza*28
 
-vza = (' ' + str(110))*55
+vza = (' ' + str(110))*65
 out_str = out_str+' '+vza
 
 #relative azimuth/meas
