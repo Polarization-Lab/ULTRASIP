7f173287a8e4e357cba0bb0609edc12c518cffed
ClarissaDeLeon <cdeleon@github.com>
2024-07-29 16:25:00 -0700

    message

--- AirMSPI_FIREXAQ/Retrievals/Aug1923/readinginsdata.py
+++ AirMSPI_FIREXAQ/Retrievals/Aug1923/readinginsdata.py
@@ -7,7 +7,7 @@ Created on Mon Jul 29 09:57:32 2024
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = open("C:/Users/ULTRASIP_1/OneDrive/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/July2524/test.txt","r")
+file = open("C:/Users/ULTRASIP_1/OneDrive/Documents/ULTRASIP/AirMSPI_FIREXAQ/Retrievals/July2524/rayleigh.txt","r")
 
 lines = file.readlines()
 sza = []
@@ -27,15 +27,21 @@ I = data[0:180]
 Q = data[180:360] #Q/I
 U = data[360:541] #U/I
 
-dolp = (np.sqrt(Q[:]**2+U[:]**2))*100
-aolp = 0.5*np.arctan2(U[:],Q[:])
+Q = Q[:]*I[:]
+U = U[:]*I[:]
+
+dolp = (np.sqrt(Q[:]**2+U[:]**2)/I[:])*100
+aolp = np.degrees(0.5*np.arctan2(U[:],Q[:]))
 
 sza = sza[0:180]
 vza = vza[0:180] #view zenith angle
 vaz = vaz[0:180] #view azimuth angle
 sca = 180-sca[0:180]
 
-plt.plot(vza[0:30],dolp[0:30])
-
+plt.plot(sca[0:30],dolp[0:30],label='DoLP [%]')
+plt.plot(sca[0:30],aolp[0:30],label='AoLP [deg]')
+plt.xlabel('Scattering Angle')
+plt.title('GRASP Forward Simulation Rayleigh Atmosphere-No Neutral Points')
+plt.legend()
 
 file.close()
